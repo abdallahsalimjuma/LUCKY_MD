e9047f16f0755e12fc1ce0ea815553f3cd3f6252
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-08-22 16:55:39 +0200

    Update chanel.js

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -4,8 +4,8 @@ const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle *LUCKY🍀_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
-    let d = ' developpé par *Fredie Tech💎*';
+    let z = 'Salut je m\'appelle *DULLAH🥀_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let d = ' developpé par *DULLAH_MD👽*';
     let varmess = z + d;
     var lien = 'https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
@@ -23,7 +23,7 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *HACKING-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+   let z ='Salut je m\'appelle *HACKING-DULLAH MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *Thomas*'
       let varmess=z+d
       var img='https://telegra.ph/file/7113ddc85a26a69a7a437.jpg'
