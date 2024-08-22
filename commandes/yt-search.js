17aaf77eea5d401a489221b2217f9c983d1565b0
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-08-22 20:01:17 +0200

    Update yt-search.js

--- commandes/yt-search.js
+++ commandes/yt-search.js
@@ -19,9 +19,9 @@ zokou({ nomCom: "yts", categorie: "Search", reaction: "💎" }, async (dest, zk,
 
     let captions = "";
 for (let i = 0; i < 15; i++) {
-  captions += `╭━━LUCKY MD V5 YOUTUBE MENU━━╮\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
+  captions += `╭━━DULLAH MD V6 YOUTUBE MENU━━╮\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
 }
-    captions += "\n======\n*powered by Fredie Tech*";
+    captions += "\n======\n*powered by DULLAH MD*";
 
     // repondre(captions)
     zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
