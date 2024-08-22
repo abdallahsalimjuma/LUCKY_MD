b961fb719fb13c538ba252cb90f1c1101e1928a3
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-08-22 18:46:14 +0200

    Update reboot.js

--- commandes/reboot.js
+++ commandes/reboot.js
@@ -14,12 +14,12 @@ const{repondre,ms,dev,superUser}=com;
 
   if(!superUser)
   {
-    return repondre("This command is for owner or ғʀᴇᴅɪᴇ ᴛᴇᴄʜ");
+    return repondre("This command is for owner or DULLAH_MD");
   }
 
   const {exec}=require("child_process")
 
-    repondre("LUCKY-MD V5 bot Restarting ⏳");
+    repondre("DULLAH-MD V6 bot Restarting ⏳");
 
   exec("pm2 restart all");
   
