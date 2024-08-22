29ded57af906c364318c0fe7f2f468ddb74946c0
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-08-22 16:15:49 +0200

    Update GroupQuotes.js

--- commandes/GroupQuotes.js
+++ commandes/GroupQuotes.js
@@ -13,13 +13,13 @@ zokou({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions)
       fetch('https://animechan.xyz/api/random')
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`*LUCKY_MD V5 Quotes*
+          repondre(`*DULLAH_MD V6 Quotes*
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *Lucky md v5*`);
+Powered by *dullah md v6*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -31,13 +31,13 @@ Powered by *Lucky md v5*`);
       fetch('https://animechan.xyz/api/random/character?name=' + query)
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`Lucky_MD v5
+          repondre(`dullah_MD v6
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *LUCKY_MD V5*`);
+Powered by *DULLUH_MD V6*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
